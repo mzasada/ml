1f7d76a590030f2a8c283e80975e49afa661d077
Maciej Zasada <maciej.zasada@allegro.pl>
2013-06-13 21:00:36 +0200

    EX-001 | gradientDescent, gradientDescentMulti, computeCostMulti, normalEq, featureNormalize

--- 001.Linear-Regression-with-Multiple-Variables/ex1/featureNormalize.m
+++ 001.Linear-Regression-with-Multiple-Variables/ex1/featureNormalize.m
@@ -7,9 +7,14 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 
 % You need to set these values correctly
 X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
+mu = mean(X);
+sigma = std(X);
 
+for row_index = 1:size(X,1)
+	for col_index = 1:size(X,2)
+		X_norm(row_index,col_index) = (X_norm(row_index,col_index) - mu(col_index)) / sigma(col_index);			
+	endfor
+endfor
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
 %               of the feature and subtract it from the dataset,
@@ -25,9 +30,6 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-
-
-
 
 
 
